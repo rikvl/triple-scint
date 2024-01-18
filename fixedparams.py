7b33c03bef52d998a9c2c6811ba835383aa4d68f
Rik van Lieshout <lieshout@cita.utoronto.ca>
2024-01-18 10:14:28 -0500

    feat: add J0437 for testing
    Also add time scales (TCB and TDB) to time parameters.

--- fixedparams.py
+++ fixedparams.py
@@ -36,6 +36,15 @@ class Target:
             ref_parallax = ref_parallax or "Deller"
             ref_inclination = ref_inclination or "Voisin"
             ref_omega_p = ref_omega_p or "Voisin"
+        elif ref_all == "J0437":
+            ref_position = ref_position or "J0437"
+            ref_proper_motion = ref_proper_motion or "J0437"
+            ref_orbital = ref_orbital or "J0437"
+            ref_parallax = ref_parallax or "J0437"
+            ref_inclination = ref_inclination or "J0437"
+            ref_omega_p = ref_omega_p or "J0437"
+        else:
+            raise ValueError(f"Unrecognized ref_all {ref_all}")
 
         self.ref_position = ref_position
         self.ref_proper_motion = ref_proper_motion
@@ -54,6 +63,9 @@ class Target:
         elif ref_proper_motion == "Voisin":
             self.mu_alpha_star = 5.01 * u.mas / u.yr
             self.mu_delta = -0.85 * u.mas / u.yr
+        elif ref_proper_motion == "J0437":
+            self.mu_alpha_star = 121.4385 * u.mas / u.yr
+            self.mu_delta = -71.4754 * u.mas / u.yr
         else:
             raise ValueError(f"Unrecognized ref_prorer_motion {ref_proper_motion}")
 
@@ -72,6 +84,12 @@ class Target:
                 pm_ra_cosdec=self.mu_alpha_star,
                 pm_dec=self.mu_delta,
             )
+        elif ref_position == "J0437":
+            self.psr_coord = SkyCoord(
+                "04h37m15.99744s -47d15m09.7170s",
+                pm_ra_cosdec=self.mu_alpha_star,
+                pm_dec=self.mu_delta,
+            )
         else:
             raise ValueError(f"Unrecognized ref_position {ref_position}")
 
@@ -80,11 +98,11 @@ class Target:
             self.p_orb_i = 1.6293932 * u.day
             self.p_orb_o = 327.25685 * u.day
 
-            self.t_asc_i = Time(55920.40771662, format="mjd")
-            self.t_asc_o = Time(56233.93512, format="mjd")
+            self.t_asc_i = Time(55920.40771662, format="mjd", scale="tcb")
+            self.t_asc_o = Time(56233.93512, format="mjd", scale="tcb")
 
-            self.asini_i = 1.2175252 * const.c * u.s
-            self.asini_o = 74.672629 * const.c * u.s
+            asini_i = 1.2175252 * const.c * u.s
+            asini_o = 74.672629 * const.c * u.s
 
             eps1_i = +6.8833e-4 * u.dimensionless_unscaled
             eps2_i = -0.91401e-4 * u.dimensionless_unscaled
@@ -104,21 +122,15 @@ class Target:
             self.t_per_i = self.t_asc_i - ma_asc_i.to_value(u.cycle) * self.p_orb_i
             self.t_per_o = self.t_asc_o - ma_asc_o.to_value(u.cycle) * self.p_orb_o
 
-            self.k_i = (
-                2 * np.pi * self.asini_i / self.p_orb_i / np.sqrt(1 - self.ecc_i**2)
-            )
-            self.k_o = (
-                2 * np.pi * self.asini_o / self.p_orb_o / np.sqrt(1 - self.ecc_o**2)
-            )
+            self.k_i = 2 * np.pi * asini_i / self.p_orb_i / np.sqrt(1 - self.ecc_i**2)
+            self.k_o = 2 * np.pi * asini_o / self.p_orb_o / np.sqrt(1 - self.ecc_o**2)
+
         elif ref_orbital == "Voisin":
             self.p_orb_i = 1.6294006 * u.day
             self.p_orb_o = 327.25539 * u.day
 
-            # self.t_asc_i = Time(55917.1584, format="mjd")  # WRONG: differently defined
-            # self.t_asc_o = Time(56230.19511, format="mjd") # WRONG: differently defined
-
-            self.asini_i = 1.2175280 * const.c * u.s
-            self.asini_o = 74.672374 * const.c * u.s
+            asini_i = 1.2175280 * const.c * u.s
+            asini_o = 74.672374 * const.c * u.s
 
             eps1_i = +6.9365e-4 * u.dimensionless_unscaled
             eps2_i = -0.8544e-4 * u.dimensionless_unscaled
@@ -132,8 +144,11 @@ class Target:
             self.arg_per_i = np.arctan2(eps1_i, eps2_i).to(u.deg) % (360 * u.deg)
             self.arg_per_o = np.arctan2(eps1_o, eps2_o).to(u.deg) % (360 * u.deg)
 
-            self.t_per_i = Time(55917.5975, format="mjd")
-            self.t_per_o = Time(56317.21976, format="mjd")
+            # Time of ascending node given in Voisin et al 2020 is defined differently
+            # so we need to compute these from the listed times of periastron passage.
+            # No time scale is listed in the paper; I have assumed TCB here.
+            self.t_per_i = Time(55917.5975, format="mjd", scale="tcb")
+            self.t_per_o = Time(56317.21976, format="mjd", scale="tcb")
 
             ma_asc_i = -th2ma(self.arg_per_i, self.ecc_i)
             ma_asc_o = -th2ma(self.arg_per_o, self.ecc_o)
@@ -141,12 +156,59 @@ class Target:
             self.t_asc_i = self.t_per_i + ma_asc_i.to_value(u.cycle) * self.p_orb_i
             self.t_asc_o = self.t_per_o + ma_asc_o.to_value(u.cycle) * self.p_orb_o
 
-            self.k_i = (
-                2 * np.pi * self.asini_i / self.p_orb_i / np.sqrt(1 - self.ecc_i**2)
-            )
-            self.k_o = (
-                2 * np.pi * self.asini_o / self.p_orb_o / np.sqrt(1 - self.ecc_o**2)
-            )
+            self.k_i = 2 * np.pi * asini_i / self.p_orb_i / np.sqrt(1 - self.ecc_i**2)
+            self.k_o = 2 * np.pi * asini_o / self.p_orb_o / np.sqrt(1 - self.ecc_o**2)
+
+        elif ref_orbital == "Parfile59575":
+            self.p_orb_i = 1.6293932 * u.day  # Archibald
+            self.p_orb_o = 327.219804954111 * u.day
+
+            asini_i = 1.217544260 * const.c * u.s
+            asini_o = 74.668594264 * const.c * u.s
+
+            self.ecc_i = 0.0006641441
+            self.ecc_o = 0.035347540
+
+            self.arg_per_i = 96.368998363183 * u.deg
+            self.arg_per_o = 95.726944226135 * u.deg
+
+            self.t_per_i = Time(55917.5975, format="mjd", scale="tcb")
+            self.t_per_o = Time(56317.21976, format="mjd", scale="tcb")
+
+            ma_asc_i = -th2ma(self.arg_per_i, self.ecc_i)
+            ma_asc_o = -th2ma(self.arg_per_o, self.ecc_o)
+
+            self.t_asc_i = self.t_per_i + ma_asc_i.to_value(u.cycle) * self.p_orb_i
+            self.t_asc_o = self.t_per_o + ma_asc_o.to_value(u.cycle) * self.p_orb_o
+
+            self.k_i = 2 * np.pi * asini_i / self.p_orb_i / np.sqrt(1 - self.ecc_i**2)
+            self.k_o = 2 * np.pi * asini_o / self.p_orb_o / np.sqrt(1 - self.ecc_o**2)
+
+        elif ref_orbital == "J0437":
+            self.p_orb_i = 5.7410459 * u.day
+            self.p_orb_o = 1 * u.day
+
+            self.t_asc_i = Time(54501.4671, format="mjd", scale="tdb")
+            self.t_asc_o = Time(54501.4671, format="mjd", scale="tdb")
+
+            asini_i = 3.3667144 * const.c * u.s
+            asini_o = 0 * const.c * u.s
+
+            self.ecc_i = 0
+            self.ecc_o = 0
+
+            self.arg_per_i = 0 * u.deg
+            self.arg_per_o = 0 * u.deg
+
+            ma_asc_i = -th2ma(self.arg_per_i, self.ecc_i)
+            ma_asc_o = -th2ma(self.arg_per_o, self.ecc_o)
+
+            self.t_per_i = self.t_asc_i - ma_asc_i.to_value(u.cycle) * self.p_orb_i
+            self.t_per_o = self.t_asc_o - ma_asc_o.to_value(u.cycle) * self.p_orb_o
+
+            self.k_i = 2 * np.pi * asini_i / self.p_orb_i / np.sqrt(1 - self.ecc_i**2)
+            self.k_o = 0 * u.km / u.s
+
         else:
             raise ValueError(f"Unrecognized ref_orbital {ref_orbital}")
 
@@ -154,6 +216,10 @@ class Target:
         if ref_parallax == "Deller":
             self.parallax_prior_mu = 0.88 * u.mas
             self.parallax_prior_sig = 0.06 * u.mas
+        elif ref_parallax == "J0437":
+            d_p = 156.79 * u.pc
+            self.parallax_prior_mu = d_p.to(u.mas, equivalencies=u.parallax())
+            self.parallax_prior_sig = 0.0001 * u.mas
         else:
             raise ValueError(f"Unrecognized ref_parallax {ref_parallax}")
 
@@ -167,6 +233,9 @@ class Target:
         elif ref_inclination == "Voisin":
             self.i_p_prior_mu = (180 - 39.251) * u.deg
             self.i_p_prior_sig = 0.014 * u.deg
+        elif ref_inclination == "J0437":
+            self.i_p_prior_mu = 137.56 * u.deg
+            self.i_p_prior_sig = 0.0001 * u.deg
         else:
             raise ValueError(f"Unrecognized ref_inclination {ref_inclination}")
 
@@ -174,6 +243,9 @@ class Target:
         if ref_omega_p == "Voisin":
             self.omega_p_prior_mu = (-44.34 * u.deg) % (360 * u.deg)
             self.omega_p_prior_sig = 0.14 * u.deg
+        elif ref_omega_p == "J0437":
+            self.omega_p_prior_mu = 207.0 * u.deg
+            self.omega_p_prior_sig = 0.0001 * u.deg
         else:
             raise ValueError(f"Unrecognized ref_omega_p {ref_omega_p}")
 
@@ -189,3 +261,7 @@ class Observatory:
             self.earth_loc = EarthLocation.of_site("Green Bank Telescope")
         elif telescope == "WSRT":
             self.earth_loc = EarthLocation("6°36′12″E", "52°54′53″N")
+        elif telescope == "Parkes":
+            self.earth_loc = EarthLocation("148°15′47″E", "32°59′52″S")
+        else:
+            raise ValueError(f"Unrecognized telescope {telescope}")
